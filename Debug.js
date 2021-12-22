3daff7b3308f4d032d55de115e2cfdcea91af3cb
Navid Mafi <navidmafi2006@gmail.com>
2021-12-22 11:02:39 +0330

    Stuff

--- Debug.js
+++ Debug.js
@@ -8,3 +8,8 @@ function loaddefaultsettings() {
     }
 }
 loaddefaultsettings();
+
+
+/*
+      <span>This is the last release of this project. <a class="tc-black" href="#" onclick="notifier.info('This project was supposed to be just an experiment for a larger project. And this was not going to be supported in the long run. Although this experiment will still be available for a while, we will not work on it anymore and will not provide support because we want to focus on our bigger projects. ',{labels: {info: 'Why'},durations : {info : 60000}})">Learn more</a></span>
+*/
